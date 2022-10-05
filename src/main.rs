bed66faec84760db5cf4540bead2449c2a2ad5cf
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-10-05 20:56:50 +0530

    fix: main

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,56 @@
-use anyhow::Ok;
-use clap::Parser;
-use anyhow::{Context, Result};
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
+    /// Authorizer url to operate on
+    url: Option<String>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// Invite users to app
+    InviteMembers {
+        /// Sets path for input file
+        #[arg(short, long, value_name = "FILE")]
+        path: PathBuf,
+    },
+}
 
-fn main() -> Result<()> {
-    let args = Cli::parse();
+fn main() {
+    let cli = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path)
-    .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(url) = cli.url.as_deref() {
+        println!("Value for url: {}", url);
+    }
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => (),
+        _ => println!("Debug mode is on"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::InviteMembers { path }) => {
+            let content = std::fs::read_to_string(&path).expect(&format!("could not read file `{}`", &path.display()));
+            for line in content.lines() {
+                println!("{}", line);
+            };
         }
-    };
-    Ok(())
+        None => {}
+    }
+
+    // Continued program logic goes here...
 }
