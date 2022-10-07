f2fef3c0b60c67fd94b639b09c2aeadba7f39670
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-10-07 11:17:19 +0530

    fix: main

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use reqwest::Client;
 use serde_json::{Value, Map};
 
@@ -5,6 +7,10 @@ use crate::utils::get_valid_emails;
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn convert<'a>(s: &'a Cow<'_, str>) -> &'a str {
+    s.as_ref()
+}
 
 pub async fn send_invitation(user_emails: Vec<Value>, is_debug_mode_on: bool) -> Result<()> {
     if is_debug_mode_on {
@@ -38,8 +44,14 @@ pub async fn send_invitation(user_emails: Vec<Value>, is_debug_mode_on: bool) ->
     let body = res.bytes().await?;
     let v = body.to_vec();
     let s = String::from_utf8_lossy(&v);
+    let str_v: Value = serde_json::from_str(convert(&s))?;
     if is_debug_mode_on {
         println!("response: {} ", s);
+    }
+    if !str_v["data"]["_invite_members"]["message"].is_null() {
+        println!("Success: {}", str_v["data"]["_invite_members"]["message"]);
+    } else if !str_v["errors"][0]["message"].is_null() {
+        println!("Error: {}", str_v["errors"][0]["message"]);
     }
     Ok(())
 }
